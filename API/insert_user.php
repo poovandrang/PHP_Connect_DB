d299230fccea8693fa8f522309653a23fdaf8f14
Poovandran G <136569530+poovandrang@users.noreply.github.com>
2024-09-03 16:14:49 +0530

    Create insert_user.php
    Insert Username and Password to the Mysql Database.

---
+++ API/insert_user.php
@@ -0,0 +1,38 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+
+
+
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "php";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Get data from POST request
+$name = $_POST['name'];
+$user_password = $_POST['password'];
+
+// Insert data into the database
+$sql = "INSERT INTO users (name, password) VALUES ('$name', '$user_password')";
+
+if ($conn->query($sql) === TRUE) {
+    echo json_encode(array("success" => true));
+} else {
+    echo json_encode(array("success" => false, "error" => $conn->error));
+}
+
+$conn->close();
+?>
