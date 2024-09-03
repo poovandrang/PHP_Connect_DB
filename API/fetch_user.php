281140f9a617e91711d5b82e957c31208abf29cc
Poovandran G <136569530+poovandrang@users.noreply.github.com>
2024-09-03 16:16:17 +0530

    Create fetch_user.php
    Retrive Data From The Mysql Database.

---
+++ API/fetch_user.php
@@ -0,0 +1,34 @@
+<?php
+
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "php";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT name,password FROM users";
+$result = $conn->query($sql);
+
+$users = array();
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $users[] = $row;
+    }
+}
+
+echo json_encode($users);
+
+$conn->close();
+?>
